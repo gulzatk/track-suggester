c4f70237e5ddbcffcc66cc8f00bd7171e7233a94
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-10-19 13:05:35 -0700

    it will updated the file to add imeges and worked with js branching

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("input#name").val();
+    var subject =$ ("#subject");
+    var learn = $("#learn");
+    var company = $("#company");
+    var languge = $("#languge");
+    var logic = $("#ligic");
+
+
+$(".name").text(name);
+
+    if (subject === "math" && logic === "back-end" ||learn === "programmingLanguge" && company === "established") {
+      $("#C#/.NET").show();
+    }
+    else if (subject === "art" && logic === "front-end" || learn === "web-pages" && languge === "CSS/Design") {
+      $("#CSS/Design").show();
+    }
+    else if (learn === "web-application" && logic === "back-end" || company === "start-up" &&  languge === "Ruby/Rails") {
+      $("#Ruby/Rails").show();
+    }  else
+    }
+  });
+});
