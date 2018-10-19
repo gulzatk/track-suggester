cfba8544df792d7fe482dbb6f99a25ce3c8caa83
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-10-19 16:55:13 -0700

    it will fixed javaScripts branching

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     var name = $("input#name").val();
-    var question1 =$ ("#question1");
-    var question2 = $("#question2");
-    var question3 = $("#question3");
-    var question4 = $("#question4");
-    var question5 = $("#question5");
+    var question1 =$ ("#question1").val();
+    var question2 = $("#question2").val();
+    var question3 = $("#question3").val();
+    var question4 = $("#question4").val();
+    var question5 = $("#question5").val();
 
 $(".name").text(name);
 
     if (question1 === "programmingLanguage" && question2 === "back-end" && question3 === "1" && question4 === "established" && question5 === "c-sharp") {
-      $("#c-sharp/.NET").show();
+      $("#c-sharp").show();
     }
     else if (question1 === "web-pages" && question2 === "front-end" && question3 === "3" && question4 === "start-up" && question5 === "CSS") {
-      $("#CSS/Design").show();
+      $("#CSS").show();
     }
     else if (question1 === "web-application" && question2 === "back-end" && question3 === "2" &&  question4 === "established" && question5 === "Ruby") {
-      $("#Ruby/Rails").show();
+      $("#Ruby").show();
     } else {
       $(".anyone").show()
     }
